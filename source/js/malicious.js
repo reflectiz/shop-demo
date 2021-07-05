558b4bba519fd4ca9717149544c97fc45c63031f
AzureAD\YakovMendelson <yakov.m@reflectiz.com>
2021-07-05 15:25:34 +0300

    change url malicious

--- source/js/malicious.js
+++ source/js/malicious.js
@@ -1,5 +1,5 @@
 function myFunction() {
-   fetch('https://iyfsearch.com/',{ mode: 'no-cors'})
+   fetch('https://form.123formbuilder.com/',{ mode: 'no-cors'})
     .then(response => console.log(response))
     .then(res=>{console.log(res); window.location.href = 'confirmation.html'})
 }
