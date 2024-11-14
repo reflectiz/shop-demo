7f8a2b73d2948490d998e0e3ac7ecdb6ff622917
AzureAD\ElieBenAyoun <elie.b@reflectiz.com>
2024-11-14 15:27:25 +0200

    added credit card number and script reading it

---
+++ source/js/readCreditCardInfo.js
@@ -0,0 +1,6 @@
+const cardNumber = document.querySelector('.debit-card-number');
+const cardExpiryDate = document.querySelector('.debit-card-valid');
+const cardCvv = document.querySelector('.debit-card-cvv');
+console.log('cardNumber',cardNumber.innerHTML);
+console.log('cardExpiryDate',cardExpiryDate.innerHTML);
+console.log('cardCvv',cardCvv.innerHTML);
